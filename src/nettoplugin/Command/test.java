e22dec673c33a35b862064b463df98871e8fffec
NETtoSan <62388769+NETtoSan@users.noreply.github.com>
2021-04-03 16:36:16 +0700

    Add files via upload

---
+++ src/nettoplugin/Command/test.java
@@ -0,0 +1,193 @@
+package nettoplugin.Command;
+
+import arc.Core;
+import arc.Events;
+import arc.util.Nullable;
+import mindustry.*;
+import mindustry.Vars;
+import mindustry.game.EventType.*;
+import mindustry.game.Team;
+import mindustry.gen.Call;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
+import mindustry.maps.Map;
+import mindustry.maps.Maps.*;
+import mindustry.maps.*;
+import mindustry.net.Administration;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import nettoplugin.mindustryCommand;
+import mindustry.core.GameState.*;
+import mindustry.Vars.*;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+
+import static mindustry.Vars.state;
+
+public class test extends ListenerAdapter {
+    private @Nullable Map nextMapOverride;
+    private JSONObject dOb = mindustryCommand.alldata.getJSONObject("discord");;
+    public Role crap;
+    public Maps maps = new Maps();
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+        String[] args = event.getMessage().getContentRaw().split(" ");
+        String roleID = dOb.getString("gameOver_role_id");
+        if(args[0].startsWith("..") && !event.getChannel().getName().contains("bot")){
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("This is not a valid channel! No matter if its a valid command or not");
+            eb.setDescription("Please use it in <#783149893622562838>.\n Set a good example!");
+            eb.setColor(0x33FFEC);
+
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage(eb.build()).queue();
+
+            return;
+        }
+        if(args[0].equalsIgnoreCase("..players")) {
+            if(state.isMenu()){
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessage("**The server is down!** Server needs to be running before you can use this command\nTo view left players. NETto!_OS should have them all. Its been stored on the disk").queue();
+                return;
+            }
+            StringBuilder lijst = new StringBuilder();
+            StringBuilder admins = new StringBuilder();
+            lijst.append("Players: total "+ Groups.player.size());
+            for(Player user: Groups.player){
+                Administration.PlayerInfo userinfo = user.getInfo();
+                admins.append(userinfo.lastName+ " / "+userinfo.admin+"\n");
+            }
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("Joined players");
+            eb.setColor(0x33FFEC);
+            eb.setDescription("This is currently under testing. Significant stuffs coming in very soon!\nTo view left players. NETto!_OS should have them all. Its been stored on the disk");
+            eb.addField("Players total", lijst.toString(),false);
+            eb.addField("Administrators",admins.toString(),false);
+
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage(eb.build()).queue();
+            return;
+        }
+        if(args[0].equalsIgnoreCase("..test")){
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage(event.getMember().getRoles().toString()).queue();
+            return;
+        }
+        if(args[0].equalsIgnoreCase("..gameover")){
+            Role validity = event.getGuild().getRoleById(roleID);
+            if(validity != null){
+                int identity = event.getMember().getRoles().indexOf(validity);
+                System.out.println(identity);
+                if(identity <= 0){
+                    event.getChannel().sendTyping().queue();
+                    event.getChannel().sendMessage("**No permission!**. Only NETtoTOWN managers can execute this command.").queue();
+                    return;
+                }
+                crap = event.getMember().getRoles().get(identity);
+                if(crap != null) {
+                    if(state.isMenu()){
+                        event.getChannel().sendTyping().queue();
+                        event.getChannel().sendMessage("**The server is down!** Server needs to be running before you can use this command").queue();
+                    }
+                    else{
+                        try{
+                            event.getChannel().sendTyping().queue();
+                            event.getChannel().sendMessage("Executing!").queue();
+                            Events.fire(new GameOverEvent(Team.crux));
+
+                        }
+                        catch(Exception e){
+                            event.getChannel().sendTyping().queue();
+                            event.getChannel().sendMessage("There was an error INSIDE a gameover event!").queue();
+                        }
+                    }
+                }
+                else{
+                    event.getChannel().sendTyping().queue();
+                    event.getChannel().sendMessage("**No permission!**. Only NETtoTOWN managers can execute this command.").queue();
+                }
+            }
+            return;
+        }
+        if(args[0].equalsIgnoreCase("..say")){
+            String[] msg = event.getMessage().getContentRaw().split(" ",2);
+            if(state.isMenu()){
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessage("**The server is down!** Server needs to be running before you can use this command").queue();
+                return;
+            }
+            Call.sendMessage("[cyan]"+event.getAuthor().getName()+"@Discord >[]"+ msg[1].trim());
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage("Sent!").queue();
+            return;
+        }
+        if(args[0].equalsIgnoreCase("..help")){
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("Help embed");
+            eb.setDescription("This embed shows you all the commands where you can interact with NETtoTOWN");
+            eb.setColor(0x33FFEC);
+            eb.addField("Public commands","Prefix: *..*\n1. players\n2. test\n3. say\n4. help",false);
+            eb.addField("Management commands","Prefix: *..*\n1. gameover",false);
+
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage(eb.build()).queue();
+            return;
+        }
+        if(args[0].equalsIgnoreCase("..host")){
+
+        }
+        if(args[0].equalsIgnoreCase("..info")){
+            if(state.isMenu()){
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessage("**The server is down!** Server needs to be running before you can use this command").queue();
+                return;
+            }
+            try{
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("Information");
+                eb.setDescription("NETtoTOWN status");
+                eb.addField("Current map",state.map.name(),false);
+                eb.addField("Performance",Core.graphics.getFramesPerSecond()+"FPS / "+ Core.app.getJavaHeap()/1024/1024+" Megabytes",false);
+                eb.setColor(0x33FFEC);
+                eb.addField("Players",""+Groups.player.size(), false);
+
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessage(eb.build()).queue();
+                return;
+            }
+            catch(Exception e){
+
+            }
+        }
+        if(args[0].equalsIgnoreCase("..maps")){
+            StringBuilder nmap = new StringBuilder();
+            EmbedBuilder eb = new EmbedBuilder();
+            int i = 1;
+            Vars.maps.reload();
+            eb.setTitle("Maps").setDescription("Maps stored to NETtoTOWN").setColor(0x33FFEC);
+            for(Map m: Vars.maps.customMaps()){
+                nmap.append("**"+i+")**"+m.name()+"("+m.width+"/"+m.height+")\n");
+
+            }
+            eb.addField("Total maps: "+i,nmap.toString(),false);
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage(eb.build()).queue();
+        }
+        else if(args[0].startsWith("..")){
+            if(event.getAuthor().isBot()) return;
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("Unknown command!");
+            eb.setDescription("Run **..help** to get a list of all commands!");
+            eb.setColor(0xFF3333);
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage(eb.build()).queue();
+            return;
+        }
+    }
+
+}
--- src/nettoplugin/mindustryCommand.java
+++ src/nettoplugin/mindustryCommand.java
@@ -1,131 +1,121 @@
 package nettoplugin;
 
-
-//javacord
-
-import arc.util.*;
-import arc.Core;
-import arc.Events;
-
+import arc.*;
+import mindustry.content.Blocks;
+import mindustry.core.GameState;
 import mindustry.game.EventType;
-
-import mindustry.gen.*;
-import mindustry.content.*;
-import mindustry.mod.*;
+import net.dv8tion.*;
+import org.javacord.*;
+import arc.Core;
 import mindustry.mod.Plugin;
+import mindustry.net.Administration.PlayerAction;
+import mindustry.game.EventType.*;
+import mindustry.gen.Player;
+import mindustry.core.GameState.*;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.ChannelType;
 import org.javacord.api.DiscordApi;
+import nettoplugin.Command.test;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.Channel;
 import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.message.MessageBuilder;
-import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.entity.permission.Role;
-
-//json
+import org.javacord.api.event.message.reaction.ReactionRemoveEvent;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-
-import java.lang.Thread;
-import java.util.HashMap;
+import static mindustry.Vars.*;
+import javax.security.auth.login.LoginException;
+import java.util.List;
 import java.util.Optional;
 
-
 public class mindustryCommand extends Plugin{
-
     private final Long CDT = 300L;
-    private final String FileNotFoundErrorMessage = "File not found! config\\mods\\settings.json";
-    private JSONObject alldata;
+    private final String FileNotFoundErrorMessage = "File not found!";
+    public static JSONObject alldata;
     private JSONObject data;
-    private DiscordApi api = null;
-    private HashMap<Long,String> cooldowns = new HashMap<Long,String>();
+    public static JDA jda;
 
+    //Automated message. Using javacord first!
+    public DiscordApi api = null;
     public mindustryCommand(){
-        Log.info("Loaded nettoplugin constructor");
-
+        System.out.println("It loaded!");
         try{
             String pureJSON = Core.settings.getDataDirectory().child("mods/settings.json").readString();
             alldata = new JSONObject(new JSONTokener(pureJSON));
-            if(!alldata.has("in-game")){
-                System.out.println("[ERR!] nettoplugin: settings.json has an invalit format!\n");
+            if(!alldata.has("in-game")) {
+                System.out.println("[ERR!] nettoplugin: settings.json has an invalid format!\n");
                 return;
-            } else {
+            }
+            else{
                 data = alldata.getJSONObject("in-game");
             }
-        } catch (Exception e){
+        }
+        catch(Exception e){
             if(e.getMessage().contains(FileNotFoundErrorMessage)){
-                System.out.println("[ERR!] nettoplugin: settings.json file is missing.\nBot cant start!");
+                System.out.println("[ERR!] nettoplugin: settings.json file is missing");
                 return;
-            } else {
+            }
+            else{
                 System.out.println("[ERR!] nettoplugin: Init error");
-                return;
+                return ;
             }
         }
         try{
-            api = new DiscordApiBuilder().setToken(alldata.getString("token")).login().join();
-        }catch(Exception e){
-            if(e.getMessage().contains("READY Packet")){
-                System.out.println("\n[ERR!] nettoplugin: invalid token.\n");
-            } else {
-                e.printStackTrace();
+            System.out.println("It loaded! and made through here!");
+            jda = JDABuilder.createDefault(alldata.getString("token")).build();
+            jda.getPresence().setStatus(OnlineStatus.ONLINE);
+            jda.getPresence().setActivity(Activity.listening("absolutely nothing"));
+            jda.addEventListener(new test());
+            try{
+                api = new DiscordApiBuilder().setToken(alldata.getString("token")).login().join();
+            }
+            catch(Exception e){
+                System.out.println("[ERROR!] javacord: Cannot login!");
             }
         }
-
-        Log.info("This has run");
-        BotThread bt = new BotThread(api,Thread.currentThread(),alldata.getJSONObject("discord"));
-        bt.setDaemon(false);
-        bt.start();
+        catch(Exception e){
+            if(e.getMessage().contains("READY Packet")){
+                System.out.println("\n[ERR!] nettoplugin: invalid token.\n");
+            }
+        }
 
         if(data.has("live_chat_channel_id")){
             TextChannel tc = this.getTextChannel(data.getString("live_chat_channel_id"));
             if(tc != null){
                 Events.on(EventType.PlayerChatEvent.class, event->{
-                   tc.sendMessage("**" + event.player.name.replace("*","+")+ "**: " + event.message);
+                    tc.sendMessage("**" + event.player.name.replace("*","+")+ "**: " + event.message);
+                });
+                Events.on(ServerLoadEvent.class,event->{
+                    if(state.isMenu()){
+                        tc.sendMessage("**STATUS** Server is closed!");
+                    }
+                    if(state.isPaused()){
+                        tc.sendMessage("**STATUS** Server is opened!");
+                    }
                 });
             }
         }
     }
     @Override
     public void init(){
-        Events.on(EventType.BuildSelectEvent.class, event ->{
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.microProcessor && event.builder.isPlayer()){
+        TextChannel tc = this.getTextChannel(data.getString("live_chat_channel_id"));
+        Events.on(BuildSelectEvent.class, event->{
+            if(!event.breaking && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
                 Player player = event.builder.getPlayer();
-
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a microprocessor at " + event.tile.x + ", " + event.tile.y);
-
-                //send to discord
-                TextChannel tc = this.getTextChannel(data.getString("live_chat_channel_id"));
-                if(tc != null){
-                    tc.sendMessage("**[ALERT!]** "+ player.name + " ("+player.uuid()+") has begun building a microprocessor at " + event.tile.x + ", " + event.tile.y);
-                }
+                tc.sendMessage("**ALERT!** "+ player.name+ " started building a reactor at " + event.tile.x + " , " + event.tile.y+"!");
+            }
+            if(event.breaking){
+                
             }
         });
-    }
-    @Override
-    public void registerServerCommands(CommandHandler handler){
-    }
-    @Override
-    public void registerClientCommands(CommandHandler handler){
-        if(api != null){
-            handler.<Player>register("d" , "<text...>","Sends a message to moderators", (args,player)->{
-                if(args[0] == "null"){
-                    player.sendMessage("[scarlet]Please enter a context!");
-                }
-                if(!data.has("dchannel_id")){
-                    player.sendMessage("[scarlet]This command is disabled!");
-                }
-                else {
-                    TextChannel tc = this.getTextChannel(data.getString("dchannel_id"));
-                    if(tc == null) {
-                        player.sendMessage("[scarlet]This command has no message configuration!");
-                        return;
-                    }
-                    tc.sendMessage(player.name + " *@Mindustry* :"+ args[0]);
-                    player.sendMessage("Sent!");
-                }
-            });
-        } else {
-            Log.err("[ERR!] API null!");
-        }
+        Events.on(PlayerAction.class, event->{
+            if(event.block.placeableOn){
+
+            }
+        });
     }
     public TextChannel getTextChannel(String id){
         Optional<Channel> dc = ((Optional<Channel>)this.api.getChannelById(id));
@@ -139,13 +129,5 @@ public class mindustryCommand extends Plugin{
             return null;
         }
         return dtc.get();
-    }
-    public Role getRole(String id){
-        Optional<Role> r1 = this.api.getRoleById(id);
-        if(!r1.isPresent()){
-            System.out.println("[ERR!] nettoplugin: adminrole not found!");
-            return null;
-        }
-        return r1.get();
     }
 }
