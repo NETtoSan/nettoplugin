3018f4ad42215970b6a8554cdb8bf16384e97a63
NETtoSan <netzamora05@gmail.com>
2021-10-20 03:48:03 +0700

    add succ

--- src/nettoplugin/Command/discordCommands.java
+++ src/nettoplugin/Command/discordCommands.java
@@ -3,12 +3,17 @@ package nettoplugin.Command;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
-
-public class discordCommands extends ListenerAdapter{
-    public void onGuildMessageReceived (GuildMessageReceivedEvent event){
+public class discordCommands extends ListenerAdapter {
+  public void onGuildMessageReceived (GuildMessageReceivedEvent event){
         String[] args = event.getMessage().getContentRaw().split(" ");
         if(args[0].equalsIgnoreCase("..")){
             return;
+        }
+        if(args[0].equalsIgnoreCase("..succ")){
+          event.getChannel().sendTyping().queue();
+          event.getChannel().sendMessage("no u").queue();
+
+          return;
         }
     }
 }
